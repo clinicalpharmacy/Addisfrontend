ea3f6beaba59d384e559535745df6a9bae156710
Eskinder Ayalew <eskinder.cpat@gmail.com>
2026-02-02 20:29:12 +0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -293,6 +293,23 @@ const Login = () => {
                         </div>
                     </div>
 
+                    {/* Help Text */}
+                    <div className="mt-6 text-center">
+                        <p className="text-xs text-gray-500">
+                            Having trouble?{' '}
+                            <button
+                                onClick={() => {
+                                    checkBackendStatus();
+                                    console.log('Current API URL:', API_URL);
+                                }}
+                                className="text-blue-600 hover:text-blue-800 underline"
+                            >
+                                Check connection
+                            </button>
+                        </p>
+                    </div>
+                </div>
+
                 {/* Footer */}
                 <div className="mt-6 text-center">
                     <p className="text-xs text-gray-500">
