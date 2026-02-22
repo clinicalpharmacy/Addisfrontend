893d2b9b9e2ae7082fec7f36944b89193384968f
Eskinder Ayalew <eskinder.cpat@gmail.com>
2026-02-22 10:34:28 +0300

    Update AdminUsefulLinks.jsx

--- src/pages/AdminUsefulLinks.jsx
+++ src/pages/AdminUsefulLinks.jsx
@@ -124,9 +124,9 @@ const AdminUsefulLinks = () => {
                                 />
                             </div>
                             <div className="md:col-span-1">
-                                <label className="block text-[10px] font-black text-gray-400 uppercase tracking-tighter mb-1.5 ml-1">Destination URL (Global) *</label>
+                                <label className="block text-[10px] font-black text-gray-400 uppercase tracking-tighter mb-1.5 ml-1">Destination URL (Global)</label>
                                 <input
-                                    type="url" required
+                                    type="url" 
                                     value={formData.url}
                                     onChange={(e) => setFormData({ ...formData, url: e.target.value })}
                                     className="w-full border border-gray-200 rounded-xl px-4 py-2.5 text-sm focus:ring-2 focus:ring-blue-500 outline-none bg-white font-mono text-[11px] font-bold"
