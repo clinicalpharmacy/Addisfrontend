c603973ca3975b855cc1f452c26beffed6ac5a5c
Eskinder Ayalew <eskinder.cpat@gmail.com>
2026-02-22 22:55:00 +0300

    Update Sidebar.jsx

--- src/components/Common/Sidebar.jsx
+++ src/components/Common/Sidebar.jsx
@@ -193,7 +193,7 @@ const Sidebar = ({ onClose }) => {
                                             }
                                         >
                                             <span className="w-2 h-2 rounded-full bg-current opacity-40" />
-                                            Patient Lists
+                                            Patient List
                                         </NavLink>
                                         <NavLink
                                             to="/patients/new"
