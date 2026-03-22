7dbf571052abd60f276e0efa63edce53740d5065
Eskinder Ayalew <eskinder.cpat@gmail.com>
2026-03-22 11:20:40 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -125,7 +125,7 @@ const Home = () => {
 
             {/* Simple Footer */}
             <div className="text-center text-gray-500 text-xs sm:text-sm">
-                <p>Addismed - Enhacing patient safety and rational medicines use</p>
+                <p>AddisMed - Enhacing patient safety and rational medicines use</p>
             </div>
         </div>
     );
