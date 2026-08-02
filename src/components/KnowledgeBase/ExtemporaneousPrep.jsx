0c69e19cfb18ed0cba646902c7c3481476dde3ab
Eskinder Ayalew <eskinder.cpat@gmail.com>
2026-08-02 15:56:22 +0300

    Increase modal width in Preparation Details

--- src/components/KnowledgeBase/ExtemporaneousPrep.jsx
+++ src/components/KnowledgeBase/ExtemporaneousPrep.jsx
@@ -1191,7 +1191,7 @@ const ExtemporaneousPrep = () => {
                 {/* Preparation Details Modal - Narrower Width with Enhanced Formatting */}
                 {selectedPrep && (
                     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-end p-4 z-50">
-                        <div className="bg-white rounded-xl shadow-2xl w-[60%] max-w-[60%] min-w-[300px] max-h-[90vh] overflow-hidden flex flex-col mr-8">
+                        <div className="bg-white rounded-xl shadow-2xl w-[75%] max-w-[75%] min-w-[300px] max-h-[90vh] overflow-hidden flex flex-col mr-8">
                             {/* Header */}
                             <div className="sticky top-0 bg-gradient-to-r from-indigo-600 to-indigo-800 text-white p-3">
                                 <div className="flex justify-between items-start">
