8ab52aedfa7e83578dde1ee33e01fe0f265d221c
Eskinder Ayalew <eskinder.cpat@gmail.com>
2026-08-02 15:21:48 +0300

    Change modal alignment from left to right

--- src/components/KnowledgeBase/MinorIllnesses.jsx
+++ src/components/KnowledgeBase/MinorIllnesses.jsx
@@ -1138,7 +1138,7 @@ const MinorIllnesses = () => {
                 {/* Illness Details Modal - Narrower Width with Enhanced Formatting */}
                 {selectedIllness && (
                     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-end p-4 z-50">
-                        <div className="bg-white rounded-xl shadow-2xl w-[60%] max-w-[60%] min-w-[300px] max-h-[90vh] overflow-hidden flex flex-col ml-auto">
+                        <div className="bg-white rounded-xl shadow-2xl w-[60%] max-w-[60%] min-w-[300px] max-h-[90vh] overflow-hidden flex flex-col mr-auto">
                             {/* Header */}
                             <div className="sticky top-0 bg-gradient-to-r from-red-600 to-red-800 text-white p-3">
                                 <div className="flex justify-between items-start">
