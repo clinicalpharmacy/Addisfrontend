3f1438151134a3b2fbe5c9d58162184499b87f86
Eskinder Ayalew <eskinder.cpat@gmail.com>
2026-08-03 00:05:32 +0300

    Update QuickSafetyCheck.jsx

--- src/pages/QuickSafetyCheck.jsx
+++ src/pages/QuickSafetyCheck.jsx
@@ -309,7 +309,7 @@ const QuickSafetyCheck = () => {
                                 <div className="flex flex-col items-center gap-4">
                                     <div>
                                         <p className="text-green-600 text-xl font-black max-w-2xl mx-auto">
-                                            No contraindication data about {result.medication} in Addis Med for its safe use in the selected condition(s)
+                                            No contraindication data found in Addis Med for safe use of {result.medication} in the selected condition(s)
                                             {selectedCategory !== 'all' && ` [${CategoryTitle({ type: selectedCategory })}]`}.
                                             Always consult with your healthcare provider.
                                         </p>
