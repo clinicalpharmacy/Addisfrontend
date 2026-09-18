328e5e322803090b728c1583a7de53b9c21537c6
Eskinder Ayalew <eskinder.cpat@gmail.com>
2026-09-18 14:25:46 +0300

    Update QuickSafetyCheck.jsx

--- src/pages/QuickSafetyCheck.jsx
+++ src/pages/QuickSafetyCheck.jsx
@@ -402,8 +402,7 @@ const QuickSafetyCheck = () => {
                                 <div className="flex flex-col items-center gap-4">
                                     <div>
                                         <p className="text-green-600 text-lg font-black max-w-2xl mx-auto">
-                                            {result.medication}ን በተመለከተ አዲስ ሜድ (Addis Med) ውስጥ {selectedCategory !== 'all' && `ለ[${CategoryTitle({ type: selectedCategory })}]`} 
-                                            የጥንቃቄ መረጃ አልተገኘም። ሁልጊዜም የጤና ባለሙያ ያማክሩ።
+                                            {result.medication}ን በተመለከተ አዲስ ሜድ (Addis Med) ውስጥ {selectedCategory !== 'all' && `ለ[${CategoryTitle({ type: selectedCategory })}]`}  የጥንቃቄ መረጃ አልተገኘም። ሁልጊዜም የጤና ባለሙያ ያማክሩ።
                                         </p>
                                     </div>
                                 </div>
